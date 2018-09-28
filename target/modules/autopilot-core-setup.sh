cd93a52a076b722a20daf6e36bea76b65895e3e8
Jake Hewitt <jake@apollorobotics.org>
2018-09-28 14:17:05 -0700

    Add PM2 startup configuration

--- target/modules/autopilot-core-setup.sh
+++ target/modules/autopilot-core-setup.sh
@@ -22,3 +22,14 @@ pip install -r requirements.txt
 echo "NODE_ENV=\"development\"
 CLOUD_URI=\"http://dev.flightservice.io\"
 VEHICLE_TYPE=\"multirotor\"" > $HOME/.env
+
+# Set up PM2
+cd $HOME/workspace/autopilot-core
+pm2 start $HOME/workspace/autopilot-core/tma-service-registry/index.js --name registry
+pm2 start $HOME/workspace/autopilot-core/tma-gateway/index.js --name gateway
+pm2 start $HOME/workspace/autopilot-core/flight-stack/mavproxy_start.sh --name mavproxy
+pm2 start $HOME/workspace/autopilot-core/flight-stack/start.sh --name flight-stack
+pm2 save && pm2 startup
+sudo env PATH=$PATH:/home/apollo/.nvm/versions/node/$(nvm current)/bin /home/apollo/.nvm/versions/node/$(nvm current)/lib/node_modules/pm2/bin/pm2 startup systemd -u apollo --hp /home/apollo
+
+exit 0
