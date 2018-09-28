0ffac7821986153342b4f3650dfefa395ef455ef
Jake Hewitt <jake@apollorobotics.org>
2018-09-28 14:17:43 -0700

    Configure GUI settings

--- target/modules/sh-env-setup.sh
+++ target/modules/sh-env-setup.sh
@@ -7,3 +7,14 @@ cd ~ && git clone https://github.com/jakehewitt/bash-settings.git .bash-settings
 cd ~ && git clone https://github.com/michaeldfallen/git-radar .git-radar
 echo 'export PATH=$PATH:$HOME/.git-radar' >> ~/.bashrc
 # echo 'export PS1="$PS1\$(git-radar --bash --fetch)"' >> ~/.bashrc
+
+## Change GUI settings
+# Enable gsettings to be changed
+PID=$(pgrep gnome-session)
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+# Turn user's screen lock off
+gsettings set org.gnome.desktop.session idle-delay 0
+# Change user's wallpaper
+gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/wallpaper.png
+
+exit 0
